7a9ba7548f314f4b53bae8027e061794fd011fdf
emiloh <emil@houlborg.dk>
2022-12-02 06:13:38 +0100

    Day 2 solved

---
+++ 2022/day02/p1.py
@@ -0,0 +1,29 @@
+with open("../inputs/day02.txt") as f:
+    lines = f.readlines()
+    sum = 0
+    for line in lines:
+        print(sum)
+        split = [x.strip() for x in line.split()]
+        print(split)
+        if split[0] == "A":
+            if split[1] == "X":
+                sum += 1 + 3
+            if split[1] == "Y":
+                sum += 2 + 6
+            if split[1] == "Z":
+                sum += 3 + 0
+        if split[0] == "B":
+            if split[1] == "X":
+                sum += 1 + 0
+            if split[1] == "Y":
+                sum += 2 + 3
+            if split[1] == "Z":
+                sum += 3 + 6
+        if split[0] == "C":
+            if split[1] == "X":
+                sum += 1 + 6
+            if split[1] == "Y":
+                sum += 2 + 0
+            if split[1] == "Z":
+                sum += 3 + 3
+    print(sum)
